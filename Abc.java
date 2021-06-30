61399933f588a7ec1b775b0dd680c39399088c12
Thrilok A <thrilok.a@ibm.com>
2021-06-30 11:03:50 +0530

    Created Xyz.java and altered Abc.java

--- Abc.java
+++ Abc.java
@@ -2,6 +2,9 @@ public class Abc
 {
              public static void main(String args[])
              {
+                           for(int i=1;i<=10;i++)
+                           {
                            System.out.println("Hello world");
+                           }
              }
 }
---
+++ Xyz.java
@@ -0,0 +1,7 @@
+public class Xyz
+{
+             public static void main(String args[])
+             {
+                           System.out.println("Hi");
+             }
+}
